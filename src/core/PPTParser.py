507c796a49f2c52d3af540ea431d7377ba0cc5db
grady <grady@unyx.io>
2023-02-07 14:09:10 -0600

    commented planning stuff

--- src/core/PPTParser.py
+++ src/core/PPTParser.py
@@ -2,9 +2,23 @@ from zipfile import ZipFile
 from pathlib import Path
 
 class PPTParser:
-    def __init__(self, filepath):
+    def __init__(self, filepath: Path):
         self.filepath = filepath
+        self.unpack_dir = None
 
-    def unpack_file(self):
+    # unzips the file specified in the constructor
+    def unpack_file(self) -> None:
         with ZipFile(self.filepath) as zFile:
-            zFile.extractall(path=(self.filepath.parent / self.filepath.stem))
+            self.unpack_dir = self.filepath.parent / self.filepath.stem
+            zFile.extractall(path=self.unpack_dir)
+
+    # returns a list of the slide filepaths from the unpacked dir
+    def get_slide_filepaths(self) -> list:
+        pass
+
+    # returns a dictionary of the text in a {title: [content]} format from the given filepath of a slide XML file
+    def parse_text_from_slide_filepath(self, slide_filepath: Path) -> dict:
+        pass
+
+
+
